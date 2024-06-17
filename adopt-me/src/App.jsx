e4cd02aaa9643902ef429c990ba7cb957ff32fe8
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2024-06-17 14:11:11 +0200

    moving prettierrc file and rewritte code with jsx convention

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,34 +1,15 @@
-import React from "react";
 import { createRoot } from "react-dom";
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Cat",
-      name: "Mulia",
-      breed: "Stray",
-    }),
-    React.createElement(Pet, {
-      animal: "Parrot",
-      name: "Stue",
-      breed: "Colorful",
-    }),
-  ]);
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" animal="Dog" breed="Havanees" />
+    <Pet name="Pepper" animal="Bird" breed="Cockatile" />
+    <Pet name="Doink" animal="Cat" breed="Mixed" />
+  </div>;
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
