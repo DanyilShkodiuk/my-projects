5c4e259649c7d194a14343ee9a0b61c7b58b28a5
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2024-06-24 17:53:14 +0200

    Adding new breed input, useState/hook func, disabled attr and make animal input clear out on change

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 const ANIMALS = ["bird", "cat", "rabbit", "reptile"];
+const BREEDS = ["Poodle"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -25,11 +27,28 @@ const SearchParams = () => {
             value={animal}
             onChange={(e) => {
               setAnimal(e.target.value);
+              setBreed("");
             }}
           >
             <option />
             {ANIMALS.map((animal) => (
               <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={BREEDS.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed}>{breed}</option>
             ))}
           </select>
         </label>
