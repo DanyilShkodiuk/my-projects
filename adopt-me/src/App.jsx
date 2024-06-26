c9f2e1495e9d69c36540599b3aaad3bc7b4c5ce5
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2024-06-26 14:44:14 +0200

    installing ReactQuery and using it in new file for fetching animals

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,18 +1,30 @@
 import { createRoot } from "react-dom/client";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+
+const queryClient = QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt ME!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />5
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt ME!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />5
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
