d7739a0a16871414173cb0ac098bf9ea24a18249
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2024-06-26 15:08:08 +0200

    using React Queries to cache data more proficiently, avoiding of useEffect usage

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
-const queryClient = QueryClient({
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
