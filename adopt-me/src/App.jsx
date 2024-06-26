285c3a152665bc5dd7d2f055d9129951db187cca
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2024-06-26 14:24:58 +0200

    Using useParams to pull id put of BrowserRouter, adding link to root page for header

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt Me!</h1>
+      <header>
+        <Link to="/">Adopt ME!</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />5
         <Route path="/" element={<SearchParams />} />
