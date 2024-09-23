682881bd855ca021f2a0e62a88f4c8ca8626a1a1
Danyil Shkodiuk <danil.shkodiuk@gmail.com>
2024-09-23 17:21:57 +0200

    small proj to practice loops and operatiors usage

---
+++ intro-in-fe-dev/js-project/experiments/project.js
@@ -0,0 +1,10 @@
+const timesToRepeat = 10;
+let character = "🐶";
+let answer = "";
+//some loop that takes that 🐶 and repeats it that many times
+
+for (let i = 0; i < timesToRepeat; i++) {
+  answer += character;
+}
+
+console.log(answer);
